68f78d6dfdfe01c446f40abe6b46e70cf3857cf4
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-08 10:19:44 +0700

    Create ternaty.dart

---
+++ ternaty.dart
@@ -0,0 +1,21 @@
+//INI ADALAH CONTOH YANG TIDAK MEMAKAI TERNARY OPERATOR
+ void main(){
+//   var nilai = 70;
+//   String katakata;
+
+//   if(nilai >= 60){
+//     katakata = 'selamat nilai anda masih bagus';
+//   }else{
+//     katakata = 'waduh harus di tingkatkan lagi nih';
+//   }
+
+//   print(katakata);
+// }
+
+//INI ADALAH CONTOH YANG MEMAKAI TERNARY OPERATOR
+// void main(){
+//   var nilai = 90;
+//   var katakata = nilai >=70 ? 'hore kamu berhasil lulus ngab':'yah tetap semangat yah anak muda';
+  
+//   print(katakata);
+// }
